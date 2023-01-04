b7aaf754ebca124725e01cf226aaa19a55717a97
jmlJunior <jm.projetos.jr@gmail.com>
2023-01-04 14:16:40 -0300

    Novas Atualizações

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -14,17 +15,18 @@ import entities.Item;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException{
 		
 		Locale.setDefault(Locale.US);
-		
-		boolean newFolder = new File("D:\\JAVA\\Temp\\csvout").mkdir();
+
+		boolean newFolder = new File("D:\\JAVA\\Temp\\ws-eclipse\\Exercício-214\\csvout").mkdir();
 		System.out.println("Directory created successfully: " + newFolder);
-		String newPath = "D:\\JAVA\\Temp\\csvout\\summary.csv";
+		String newPath = "D:\\JAVA\\Temp\\ws-eclipse\\Exercício-214\\CSVOut\\summary.csv";
+		System.out.println("File created successfully! ");
 		
 		List<Item> list = new ArrayList<>();
 		
-		String path = "D:\\JAVA\\Temp\\vgsales.csv";
+		String path = "D:\\JAVA\\Temp\\ws-eclipse\\Exercício-214\\csvin\\vgsales.csv";
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(path))){
 			
@@ -34,26 +36,22 @@ public class Program {
 				String[] item = line.split(",");
 				String p = item[1];
 				String pr = item[5];
-				String na = item[6];
-				String eu = item[7];
-				String jp = item[8];
-				String ot = item[9];
-				String gbl = item[10];
-				
-				double p1 = Double.parseDouble(na);
-				double p2 = Double.parseDouble(eu);
-				double p3 = Double.parseDouble(jp);
-				double p4 = Double.parseDouble(ot);
-				double p5 = Double.parseDouble(gbl);
-				double avg = (p1+p2+p3+p4+p5)/5;
+				double na = Double.parseDouble(item[6]);
+				double eu = Double.parseDouble(item[7]);
+				double jp = Double.parseDouble(item[8]);
+				double ot = Double.parseDouble(item[9]);
+				double gbl = Double.parseDouble(item[10]);
+				double avg = (na+eu+jp+ot+gbl)/5;
 				
 				list.add(new Item(p, pr, avg));
 				line = br.readLine();
 			}
 			
 			for (Item i: list) {
-				
-				String[] lines = new String[] {i.getName() + "," + i.getPublisher() + "," + String.format("%.2f",i.getAverage())};
+				String[] lines = new String[] {i.getName() + "," 
+				                                           + i.getPublisher() 
+				                                           + "," 
+				                                           + String.format("%.2f",i.getAverage())};
 				
 				try (BufferedWriter bw = new BufferedWriter(new FileWriter(newPath, true))){
 					for (String newLines : lines) {
@@ -67,10 +65,6 @@ public class Program {
 		}
 		
 		catch (IOException e) {
-			System.out.println("ERRO: " + e.getMessage() + "!");
-		}
-		
-		catch (NumberFormatException e) {
 			System.out.println("ERRO: " + e.getMessage() + "!");
 		}
 	}
